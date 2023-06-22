76b35231f559a24bb1f677e921a491e37481743d
Matheus M <matheus150101miranda@gmail.com>
2023-06-22 10:25:19 -0300

    rename folders

--- src/components/FormRegister/index.tsx
+++ src/components/FormRegister/index.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "./styles";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Input from "../Input";
-import ModalRegister from "../Modal/Register";
+import ModalRegister from "../Modais/Register";
 import { useState } from "react";
 import { keyboardKey } from "@testing-library/user-event";
 import { iProgressProps, iUserRegister } from "../../interface";
