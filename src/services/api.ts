97f4ee025327110407d83b06ea964a469b7c0c58
Matheus M <matheus150101miranda@gmail.com>
2023-06-23 10:01:09 -0300

    add api in service

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const token = localStorage.getItem("RPlace:Token");
 
 export const Api = axios.create({
-  baseURL: "http://localhost:3001",
+  // baseURL: "http://localhost:3001",
+  baseURL: "recruiters-place.up.railway.app",
   timeout: 2500,
   headers: {
     Authorization: `Bearer ${token}`,
